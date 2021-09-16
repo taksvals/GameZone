3f88bf5bf9ee6eaf0978f4713f012925278f8ea7
Tetiana Slavska <tslavska01@gmail.com>
2021-09-16 11:03:35 +0300

    App

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import AppLoading from 'expo-app-loading';
+
+import * as Font from 'expo-font';
+
+import Navigator from './routes/drawer';
+
+const getFonts = () => Font.loadAsync({
+  'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+  'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded){
+    return (
+      <Navigator />
+    );
+  } else {
+	return (
+		<AppLoading 
+			startAsync={getFonts}
+			onFinish={() => setFontsLoaded(true)}
+			onError={() => console.log('Something went wrong.')}
+    />
+	);
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
