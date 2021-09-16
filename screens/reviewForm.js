938ea6fd8a89821f2dd7de064daf4c82fdbb681c
Tetiana Slavska <tslavska01@gmail.com>
2021-09-16 11:21:52 +0300

    Validation Update

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -3,28 +3,36 @@ import { StyleSheet, View, Text, TextInput, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 import FlatButton from "../shared/button";
 import { Formik } from "formik";
-import * as yup from 'yup';
 
-const reviewSchema = yup.object({
-    title: yup.string()
-        .required()
-        .min(4),
-    body: yup.string()
-        .required()
-        .min(8),
-    rating: yup.string()
-        .required()
-        .test('is-num-1-5', 'Rating must be a number 1-5', (val) => {
-            return parseInt(val) < 6 && parseInt(val) > 0;
-        })
-});
+const validate = ({ title, body, rating }, props) => {
+    const errors = {};
+  
+    if (!title) {
+      errors.title = 'Field is Required';
+    } else if (title.length < 4) {
+      errors.title = 'To short! Add at least 4 characters';
+    }
+
+    if (!body) {
+        errors.body = 'Field is Required';
+      } else if (body.length < 8) {
+        errors.body = 'To short! Add at least 8 characters';
+      }
+
+      if (!rating) {
+        errors.rating = 'Field is Required';
+      } else if (parseInt(rating) >= 6 || parseInt(rating) <= 0) {
+        errors.rating = 'Rating must be a number 1-5';
+      }
+    return errors;
+};
 
 export default function ReviewForm({ addReview }) {
     return(
         <View style={globalStyles.container}>
             <Formik
                 initialValues={{ title: '', body: '', rating: '' }}
-                validationSchema={reviewSchema}
+                validate={validate}
                 onSubmit={(values, actions) => {
                     actions.resetForm();
                     addReview(values);
